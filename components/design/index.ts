4bcea3c3c8c9d4df404b10917d5c68f0f56de70d
Brumalia <brumalia@agentmail.to>
2026-02-15 11:34:42 +0000

    fix: re-export StatusPanel from design index to simplify imports

--- components/design/index.ts
+++ components/design/index.ts
@@ -1,6 +1,4 @@
 export { Button } from './Button';
 export { Card, CardHeader, CardContent } from './Card';
 export { Badge } from './Badge';
-export { Spinner } from './Spinner';
-export { StatusBar, StatusPanel, StatusBarWithPanel } from './StatusBar';
-export type { StatusBarProps, StatusPanelProps, HealthStatus } from './StatusBar';
+export { StatusPanel } from './StatusPanel';
